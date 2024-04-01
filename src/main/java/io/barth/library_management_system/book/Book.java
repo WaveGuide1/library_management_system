06313732e35ce6ff4a14afffb8b77643761f959d
WaveGuide1 <barthalaekwe@gmail.com>
2024-04-01 21:40:12 +0100

    I implemented custom validation for ISBN

--- src/main/java/io/barth/library_management_system/book/Book.java
+++ src/main/java/io/barth/library_management_system/book/Book.java
@@ -1,9 +1,11 @@
 package io.barth.library_management_system.book;
 
+import io.barth.library_management_system.utility.ISBN;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -14,12 +16,15 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Book must have a title")
     private String title;
 
+    @NotBlank(message = "Book must have an Author")
     private String author;
 
     private int publicationYear;
 
+    @ISBN
     private String isbn;
 
 }
---
+++ src/main/java/io/barth/library_management_system/utility/ISBN.java
@@ -0,0 +1,19 @@
+package io.barth.library_management_system.utility;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ISBNValidator.class)
+public @interface ISBN {
+
+    String message() default "Invalid ISBN";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
