af138f181d671802ffed721ae6d7b140d6bf7991
WaveGuide1 <barthalaekwe@gmail.com>
2024-04-02 14:15:51 +0100

    Borrowing record service

---
+++ src/main/java/io/barth/library_management_system/borrowingBook/BorrowingRecordController.java
@@ -0,0 +1,9 @@
+package io.barth.library_management_system.borrowingBook;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/")
+public class BorrowingRecordController {
+}
