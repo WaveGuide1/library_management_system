c2d42ca74d9ab1fec577544771c943e9cc9f2cfb
WaveGuide1 <barthalaekwe@gmail.com>
2024-04-03 16:52:48 +0100

    User model

---
+++ src/main/java/io/barth/library_management_system/authentication/User.java
@@ -0,0 +1,65 @@
+package io.barth.library_management_system.authentication;
+
+import io.barth.library_management_system.utility.Role;
+import jakarta.persistence.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Override
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/io/barth/library_management_system/utility/Role.java
@@ -0,0 +1,11 @@
+package io.barth.library_management_system.utility;
+
+public enum Role {
+
+    USER,
+    ADMIN;
+
+    public String getRole() {
+        return this.getRole();
+    }
+}
